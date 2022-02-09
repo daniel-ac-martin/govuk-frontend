5f5c771908a1476f9cfbff6b4eb7dcd0c4783fbf
owenatgov <owen.jones@digital.cabinet-office.gov.uk>
2022-02-09 17:09:50 +0000

    Add hide this page component

---
+++ src/govuk/components/hide-this-page/hide-this-page.js
@@ -0,0 +1,27 @@
+function HideThisPage ($module) {
+  this.$module = $module
+}
+
+HideThisPage.prototype.openNewTab = function (e) {
+  e.preventDefault()
+  const target = e.target
+
+  window.open(target.getAttribute('data-new-tab-url'), '_newtab')
+
+  // Check if the user's browser supports the history API
+  // If so, we apply a history scrambling enhancement
+  if (typeof window.history !== 'undefined' && typeof window.history === 'object') {
+    document.title = target.getAttribute('data-fake-page-title')
+    // Do not remove these 2 empty values. They are required to ensure we comply with the history API
+    // https://developer.mozilla.org/en-US/docs/Web/API/History_API
+    window.history.replaceState(null, '')
+  }
+
+  window.location.href = target.href
+}
+
+HideThisPage.prototype.init = function () {
+  this.$module.querySelector('.govuk-js-hide-this-page-button').addEventListener('click', this.openNewTab)
+}
+
+export default HideThisPage
